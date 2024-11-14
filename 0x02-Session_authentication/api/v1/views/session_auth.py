2f812b1cfdeef8ec06921542c6a348ae998fe704
Philip Ajuong Deng <philipajuong@gmail.com>
2024-11-14 18:29:59 +0000

    0x02. Session authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """View for session authentication
 """
+
 from flask import request, jsonify, abort
 from api.v1.views import app_views
 from models.user import User
@@ -24,16 +25,16 @@ def login():
         users = User.search({'email': email})
     except Exception:
         return jsonify({"error": "no user found for this email"}), 404
+
     if not users:
         return jsonify({"error": "no user found for this email"}), 404
 
-    for user in users:
-        if not user.is_valid_password(password):
-            return jsonify({"error": "wrong password"}), 401
-
-    from api.v1.app import auth
-
     user = users[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth  # Import here to avoid circular import issues
+
     session_id = auth.create_session(user.id)
     response = jsonify(user.to_json())
 
@@ -43,12 +44,13 @@ def login():
     return response
 
 
-@app_views.route('auth_session/logout/',
-                 methods=['DELETE'], strict_slashes=False)
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
 def logout():
     """Logout user
     """
-    from api.v1.app import auth
+    from api.v1.app import auth  # Import here to avoid circular import issues
+
     if not auth.destroy_session(request):
         abort(404)
+        
     return jsonify({}), 200
