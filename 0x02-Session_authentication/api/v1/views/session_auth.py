9282e942ddf045085b6506588d973a5f4243b0f6
Philip Ajuong Deng <philipajuong@gmail.com>
2024-11-15 17:52:55 +0000

    0x02. Session authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 """View for session authentication
 """
+
+
 from flask import request, jsonify, abort
 from api.v1.views import app_views
 from models.user import User
 from os import getenv
 import uuid
 
+
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def auth_session_login():
     """Handles user login through session authentication."""
-     return jsonify({"message": "Login route"}), 200
+    return jsonify({"message": "Login route"}), 200
     # Validate email
     email = request.form.get('email')
     if not email:
