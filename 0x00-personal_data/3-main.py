87f516e3a07a5e5f18a76ea0f9b5548b3114b507
Philip Ajuong Deng <philipajuong@gmail.com>
2024-11-30 18:46:17 +0200

    0x00. Personal data

--- 0x00-personal_data/3-main.py
+++ 0x00-personal_data/3-main.py
@@ -5,10 +5,11 @@ Main file
 
 get_db = __import__('filtered_logger').get_db
 
-db = get_db()
-cursor = db.cursor()
-cursor.execute("SELECT COUNT(*) FROM users;")
-for row in cursor:
-    print(row[0])
-cursor.close()
-db.close()
+db = get_db()  # Get the database connection
+cursor = db.cursor()  # Create a cursor to interact with the database
+cursor.execute("SELECT COUNT(*) FROM users;")  # Execute the SQL query
+for row in cursor:  # Fetch the result
+    print(row[0])  # Print the count of users
+cursor.close()  # Close the cursor
+db.close()  # Close the database connection
+
